5755ff5009c68de13df672bfeef6630353bb5e22
RohitAkurdekar <rakurdekar@gmail.com>
2023-01-18 09:45:49 +0530

    subtract feature

--- calculator.py
+++ calculator.py
@@ -8,9 +8,10 @@ def add(num1,num2):
 def sub(num1,num2):
     return num1-num2
 
-
+# call add function
 res_add = add(52,15)
 print(res_add)
 
+# call sub function
 res_sub = sub(52,15)
 print(res_sub)
