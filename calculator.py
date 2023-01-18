f53f5cb644475446d13c2662929e5f8174dd07b6
RohitAkurdekar <rakurdekar@gmail.com>
2023-01-18 10:11:04 +0530

    sub and div feature

--- calculator.py
+++ calculator.py
@@ -8,6 +8,14 @@ def add(num1,num2):
 def sub(num1,num2):
     return num1-num2
 
+# Multiplication function
+def mul(num1,num2):
+    return num1*num2
+
+# Division function
+def div(num1,num2):
+    return num1/num2
+
 # call add function
 res_add = add(52,15)
 print(res_add)
@@ -15,3 +23,11 @@ print(res_add)
 # call sub function
 res_sub = sub(52,15)
 print(res_sub)
+
+# call mul function
+res_mul = mul(52,10)
+print(res_mul)
+
+# call div function
+res_div = div(52,15)
+print(res_div)
