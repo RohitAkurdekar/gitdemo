d23409640ce654491fb55f1a6573f012ffae5b9c
RohitAkurdekar <rakurdekar@gmail.com>
2023-01-18 09:40:14 +0530

    Calculator app demo sub func

--- calculator.py
+++ calculator.py
@@ -1,6 +1,13 @@
-#!//usr/bin/python3
+#!/usr/bin/python3
 def add(num1,num2):
     return num1+num2
 
+def sub(num1,num2):
+    return num1-num2
+
+
 res_add = add(52,15)
 print(res_add)
+
+res_sub = sub(52,15)
+print(res_sub)
